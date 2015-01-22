ca8a1861f7e2f57437b1d7d8d787b1bda410a3ed
Klaha <klaha.77@gmail.com>
2015-01-22 15:05:11 -0430

    Cambios en Listado

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :users
-  resources :movies
-  
-  # Rutas Custom
-  get '/adminn/peliculas' => 'movies#new'
 
   namespace :employee do
     resources :shows
+  end
+
+  namespace :admin do
+    resources :movies
   end
   
 end
