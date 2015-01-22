45ef8e3b468e94d9a3f9ff0c41b4bc645af021c6
sonic182 <johander1822@gmail.com>
2015-01-22 14:51:18 -0430

    terminado employee/shows sin validaciones

--- app/controllers/employee/shows_controller.rb
+++ app/controllers/employee/shows_controller.rb
@@ -1,26 +1,48 @@
 class Employee::ShowsController < ApplicationController
-
+  before_action :set_show, only: [:show, :edit, :update, :destroy]
   def index
     @shows = Show.all
   end
 
   def new
     @show = Show.new
+    @movies = Movie.all
+    @path = employee_shows_path
   end
 
   def show
-    set_show
   end
 
   def create
     @show = Show.new(show_params)
-
+    @show.date_show = Date.today
     respond_to do |format|
       if @show.save
         format.html { redirect_to employee_show_path @show.id, notice: 'Show was successfully created.' }
         format.json { render :show, status: :created, location: @show }
       else
         format.html { render :new }
+        format.json { render json: @show.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @show.destroy
+    redirect_to employee_shows_path
+  end
+
+  def edit
+    @path = employee_show_path
+  end
+
+  def update 
+    respond_to do |format|
+      if @show.update(show_params)
+        format.html { redirect_to employee_shows_path, notice: 'Show was successfully updated.' }
+        format.json { render :show, status: :ok, location: @show }
+      else
+        format.html { render :edit }
         format.json { render json: @show.errors, status: :unprocessable_entity }
       end
     end
@@ -37,7 +59,7 @@ class Employee::ShowsController < ApplicationController
       # movie_id
       # time
       # date_show
-      params.require(:show).permit(:movie_id,:time, :date_show)
+      params.require(:show).permit(:movie_id,:time)
     end
 
 end
