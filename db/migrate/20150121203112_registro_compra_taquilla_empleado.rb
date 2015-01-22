4d6dba2b66590e66e2ccb6329484d03543646dcc
sonic182 <johander1822@gmail.com>
2015-01-21 19:51:50 -0430

    modelos show theater transaction

---
+++ db/migrate/20150121203112_registro_compra_taquilla_empleado.rb
@@ -0,0 +1,28 @@
+class RegistroCompraTaquillaEmpleado < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      t.integer :movie_id, index: true
+      t.time :time
+      t.date :date_show
+
+      t.timestamps
+    end
+
+    create_table :theaters do |t|
+      t.integer :theater_number
+      t.string :type
+
+      t.timestamps
+    end
+
+
+    create_table :transactions do |t|
+      t.integer :user_id, index: true
+      t.integer :show_id, index: true
+      t.timestamps
+    end
+
+    #add_foreign_key :articles, :authors
+
+  end
+end
