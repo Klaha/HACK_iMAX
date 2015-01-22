53a4851e005fc4faabc782b14e8212dff1db373a
Klaha <klaha.77@gmail.com>
2015-01-22 15:22:49 -0430

    :dog: YAJAYAJU

--- db/schema.rb
+++ db/schema.rb
@@ -37,13 +37,6 @@ ActiveRecord::Schema.define(version: 20150121203112) do
     t.datetime "updated_at"
   end
 
-  create_table "transactions", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "show_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.integer  "ci"
