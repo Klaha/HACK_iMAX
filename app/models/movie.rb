a6131997428e32b9e889f908126f4320e32d0bad
sonic182 <johander1822@gmail.com>
2015-01-25 06:41:38 -0430

    Validacioens en los modelos

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
   has_many :shows
+  validates :name, presence: true
+  validates :sinopsis, presence: true
+  validates :duration, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :transactions
+	
+	validates :name, presence: true
+	validates :ci, presence: true
+	validates :email, presence: true
+	validates :password, presence: true
+	validates :role, presence: true
 
 	def self.authenticate (params)
 		u = User.where("email = ? AND password = ?",params[:email],params[:password])
