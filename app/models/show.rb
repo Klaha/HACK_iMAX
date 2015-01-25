264f3c3f5628389a655448cca65ea54134b962d2
sonic182 <johander1822@gmail.com>
2015-01-25 15:35:10 -0430

    validaciones modelo show

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
   has_many :transactions
   belongs_to :theater
   belongs_to :movie
+  validates :movie_id, presence: true
+  validates :time, presence: true
+  validates :theater_id, presence: true
+  validates :date_show, presence: true
 end
