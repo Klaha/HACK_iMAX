bbaf03980fd3d628db25e4b213f1240906650266
sonic182 <johander1822@gmail.com>
2015-01-25 07:55:09 -0430

    corrigiendo modelos

--- app/models/seat_show.rb
+++ app/models/seat_show.rb
@@ -1,5 +1,5 @@
 class Seat_show < ActiveRecord::Base
   belongs_to :seat
   belongs_to :show
-  belongs_to :ticket
+  belongs_to :ticket_transaction
 end
--- app/models/ticket_transaction.rb
+++ app/models/ticket_transaction.rb
@@ -1,4 +1,4 @@
-class Ticket < ActiveRecord::Base
+class Ticket_transaction < ActiveRecord::Base
   has_one :seat_show
   belongs_to :transaction
 end
