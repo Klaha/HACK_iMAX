4397031ab775352c9cede4c78269974eb94f5995
Klaha <klaha.77@gmail.com>
2015-01-25 19:53:41 -0430

    Cambios en las migraciones-

--- db/migrate/20150123185343_create_transactions.rb
+++ db/migrate/20150123185343_create_transactions.rb
@@ -3,6 +3,9 @@ class CreateTransactions < ActiveRecord::Migration
     create_table :transactions do |t|
       t.integer :user_id
       t.integer :shows_id
+      t.references :user, index: true
+      t.references :show, index: true
+      t.datetime :datetime_transaction
 
       t.timestamps
     end
