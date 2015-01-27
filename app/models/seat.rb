af6ec72926bfeaa4ea327e3b55bd5ef30b311724
sonic182 <johander1822@gmail.com>
2015-01-26 22:03:29 -0430

    Fix relaciones

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,3 +1,4 @@
 class Seat < ActiveRecord::Base
+  has_many :seat_shows
   has_many :shows, through: :seat_shows
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   has_many :show_transactions
   belongs_to :theater
   belongs_to :movie
+  has_many :seat_shows
   has_many :seats, through: :seat_shows
   validates :movie_id, presence: true
   validates :time, presence: true
