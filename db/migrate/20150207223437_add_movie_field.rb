690f69e055f99982963dea2b74537224ed9c1af9
sonic182 <johander1822@gmail.com>
2015-02-07 18:21:57 -0430

    layout SHIT

---
+++ db/migrate/20150207223437_add_movie_field.rb
@@ -0,0 +1,11 @@
+class AddMovieField < ActiveRecord::Migration
+  def change
+    change_table :movies do |t|
+      t.string :img_carrousel
+      # t.remove :description, :name
+      # t.string :part_number
+      # t.index :part_number
+      # t.rename :upccode, :upc_code
+    end
+  end
+end
